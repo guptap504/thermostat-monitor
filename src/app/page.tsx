847a259bc9d2f3139f6eae69614530786ff1aef7
Prashant Gupta <prashant@quadrical.ai>
2025-02-03 11:00:13 +0530

    formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
         }
     }, [editing])
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-2 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-2 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center">
                 <Image src="/logo.png" alt="Thermostat Logo" width={120} height={40} className="h-10 w-auto" priority />
                 <div className="px-2 w-80 lg:w-[450px]">
