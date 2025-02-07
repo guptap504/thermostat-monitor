11b86fc7684ed37d1d3ac589d2eb58530747a685
Prashant Gupta <prashant@quadrical.ai>
2025-02-07 10:56:32 +0530

    changed tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
                     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-2 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
                         <main className="flex flex-col gap-8 row-start-2 items-center">
                             {/* updated logo  */}
-                            <img src="/logo.png" alt="Thermostat Logo" className="h-10 w-auto" />
+                            <Image src="/logo.png" alt="Thermostat Logo" className="h-10 w-auto" />
                             {children}
                         </main>
                         <Toaster />
